ecf59bd4f6746f41d846b1bc9ee79b7947e2971b
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-28 09:42:17 +0900

    Initial Commit

---
+++ code_test_study/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  code_test_study
+//
+//  Created by 황재민 on 10/28/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
